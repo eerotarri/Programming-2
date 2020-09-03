24b88d60cc76598fba12be19796dc60ab90b9bf3
Eero Tarri <eero.tarri@tuni.fi>
2020-09-03 10:27:32 +0300

    Lisätty myös 7:llä jaollisten lukujen käsittely

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -10,9 +10,12 @@ int main()
 
     int i = 1;
 
-    while ( i < amount + 1 )
-    {
-        if ( i % 3 == 0 ) {
+    while ( i < amount + 1 ) {
+        if ( i % 3 == 0 && i % 7 == 0) {
+            cout << "zip boing" << endl;
+        } else if ( i % 7 == 0 ) {
+            cout << "boing" << endl;
+        } else if ( i % 3 == 0 ) {
             cout << "zip" << endl;
         } else {
             cout << i << endl;
