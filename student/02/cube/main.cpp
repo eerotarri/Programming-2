0a63f043e87a7f0b848a0093d6871419e72349ca
Eero Tarri <eero.tarri@tuni.fi>
2020-09-03 14:49:04 +0300

    Kuutioonkorotusfunktio

---
+++ student/02/cube/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int cube(int num)
+{
+    int ans = num * num * num;
+    return ans;
+}
+
+int main()
+{
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    // cout << number << endl;
+
+}
