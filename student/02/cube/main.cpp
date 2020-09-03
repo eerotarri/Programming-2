bab4cd1741b841d7fbf3215af9a7a210076aa6ef
Eero Tarri <eero.tarri@tuni.fi>
2020-09-03 15:00:07 +0300

    Valmis kuutiointiohjelma

--- student/02/cube/main.cpp
+++ student/02/cube/main.cpp
@@ -15,5 +15,15 @@ int main()
     cin >> number;
 
     // cout << number << endl;
-
+    int cubed = cube(number);
+    // cout << cubed << endl;
+    if ( ( ( cubed / number ) / number ) == number ) {
+        cout << "The cube of "
+             << number << " is "
+             << cubed << endl;
+    } else {
+        cout << "Error! The cube of "
+             << number << " is not "
+             << cubed << "." << endl;
+    }
 }
