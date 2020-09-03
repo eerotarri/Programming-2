e9d14cc9302491b8d7ee61db6584619989f6700f
Eero Tarri <eero.tarri@tuni.fi>
2020-09-03 15:06:04 +0300

    Cube

--- student/02/cube/main.cpp
+++ student/02/cube/main.cpp
@@ -14,13 +14,11 @@ int main()
     cout << "Enter a number: ";
     cin >> number;
 
-    // cout << number << endl;
     int cubed = cube(number);
-    // cout << cubed << endl;
     if ( ( ( cubed / number ) / number ) == number ) {
         cout << "The cube of "
              << number << " is "
-             << cubed << endl;
+             << cubed << "." << endl;
     } else {
         cout << "Error! The cube of "
              << number << " is not "
