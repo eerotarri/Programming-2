965743e6a7fe6972721a595b68d77efba8ea22d8
Eero Tarri <eero.tarri@tuni.fi>
2020-09-03 10:41:07 +0300

    Lisätty syötteen vastaanottaminen

---
+++ student/02/mean/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+// Write here a function counting the mean value
+
+int main()
+{
+    int amount;
+    cout << "From how many integer numbers you want to count the mean value? ";
+    cin >> amount;
+
+    double num1;
+    double num2;
+    cout << "Input 1. number: ";
+    cin >> num1;
+    cout << "Input 2. number: ";
+    cin >> num2;
+}
