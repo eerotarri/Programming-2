933bbe279e997320f61b23fcfaaf217306dcd6b0
Eero Tarri <eero.tarri@tuni.fi>
2020-09-04 12:19:57 +0300

    Virheentarkastelu lisätty

--- student/02/lotto/main.cpp
+++ student/02/lotto/main.cpp
@@ -4,13 +4,17 @@ using namespace std;
 
 int main()
 {
-    unsigned long int total_balls;
+    long int total_balls;
     cout << "Enter the total number of lottery balls: ";
     cin >> total_balls;
 
-    unsigned long int drawn_balls;
+    long int drawn_balls;
     cout << "Enter the number of drawn balls: ";
     cin >> drawn_balls;
 
-    cout << total_balls << " " << drawn_balls << endl;
+    if ( ( total_balls <= 0 ) || ( drawn_balls <= 0 ) ) {
+        cout << "The number of balls must be a positive number" << endl;
+    } else {
+        // cout << total_balls << " " << drawn_balls << endl;
+    }
 }
