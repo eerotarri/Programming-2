09f6d13a1a96deaffb8b4288e2ac08af87b5ffb3
Eero Tarri <eero.tarri@tuni.fi>
2020-09-24 17:28:55 +0300

    Pelin alkutilanne luotu

---
+++ student/04/path/main.cpp
@@ -0,0 +1,107 @@
+/* Path
+ *
+ * Desc:
+ *  This program generates a Path puzzle. The board is ROWS x COLUMNS (5x4),
+ * and every round the player chooses a source point and a target point
+ * (totally four numbers), making the given button to be moved to the target
+ * position (if possible). The game checks if the user-given move can be made.
+ *  At start all green buttons are on the top row, and all red buttons are on
+ * the bottom row. Game will end when all green buttons have been moved to the
+ * bottom row, and all red buttons to the top row. It is not allowed to move a
+ * button over another button.
+ *  When printing the game board, green button is represented by 'G', red
+ * button by 'R', empty hole by 'o', and unused position by empty space.
+ *
+ * Program author
+ * Name: Eero Tarri
+ * Student number: 283568
+ * UserID: tarri
+ * E-Mail: eero.tarri@tuni.fi
+ * Feedback language (fi/en): fi
+ *
+ * Notes about the program and it's implementation:
+ *
+ * */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+enum Slot_type {RED, GREEN, EMPTY, UNUSED};
+const unsigned int ROWS = 5;
+const unsigned int COLUMS = 4;
+
+const string INPUT_TEXT = "Enter start point (x, y) and destination point (x, y), or q to quit: ";
+const string INVALID_POINT = "Invalid start/destination point.";
+const string CANNOT_MOVE = "There is no path between start point and destination point.";
+const string GAME_OVER = "Game over. Congratulations!";
+const string MOVES_MADE = " move(s) made.";
+
+// Prints the grid
+void print(const vector< vector< Slot_type >>& g)
+{
+    cout << "===============" << endl;
+    cout << "|   | 1 2 3 4 |" << endl;
+    cout << "---------------" << endl;
+    for(unsigned int i = 0; i < ROWS; ++i)
+    {
+        cout << "| " << i + 1 << " | ";
+        for(unsigned int j = 0; j < COLUMS; ++j)
+        {
+            switch(g.at(i).at(j))
+            {
+            case GREEN: cout << "G "; break;
+            case RED: cout << "R "; break;
+            case EMPTY: cout << "o "; break;
+            case UNUSED: cout << "  "; break;
+            }
+        }
+        cout << "|" << endl;
+    }
+    cout << "===============" << endl;
+}
+
+// Converts the given numeric string to the corresponding integer
+// (by calling stoi).
+// If the given string is not numeric, returns 0.
+unsigned int stoi_with_check(const string& str)
+{
+    bool is_numeric = true;
+    for(unsigned int i = 0; i < str.length(); ++i)
+    {
+        if(not isdigit(str.at(i)))
+        {
+            is_numeric = false;
+            break;
+        }
+    }
+    if(is_numeric)
+    {
+        return stoi(str);
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+// More functions
+
+int main()
+{
+    vector< vector< Slot_type > > playground = {
+        {GREEN, GREEN, GREEN, GREEN},
+        {UNUSED, EMPTY, UNUSED, UNUSED},
+        {UNUSED, EMPTY, EMPTY, UNUSED},
+        {UNUSED, EMPTY, UNUSED, UNUSED},
+        {RED, RED, RED, RED}
+    };
+
+    print(playground);
+    cout << INPUT_TEXT << endl;
+
+
+
+    return 0;
+}
