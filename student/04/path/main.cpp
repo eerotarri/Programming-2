1e2562fddbe8edc26dd6cdff976dbdd8cfd545c2
Eero Tarri <eero.tarri@tuni.fi>
2020-09-25 15:23:42 +0300

    Lisätty funktio, joka tarkastaa, onko vapaata reittiä olemassa.

--- student/04/path/main.cpp
+++ student/04/path/main.cpp
@@ -90,7 +90,7 @@ unsigned int stoi_with_check(const string& str)
 
 // Turns the numbers in input into a vector
 // by searching the first separator (" ").
-// and saving everything before it to a vector.
+// and .push_back() everything before it to a vector.
 // Returns the vector containing the input values as strings.
 vector< string > split(string text)
 {
@@ -114,15 +114,132 @@ vector< string > split(string text)
 // Takes the user input as parameter.
 // Turns the user input into a vector or integer values.
 // Returns the vector of input values.
-vector< int > get_input(string line)
+vector< unsigned int > get_input(string line)
 {
     vector< string > separated = split(line);
-    vector< int > values;
+    vector< unsigned int > values;
 
     for ( unsigned int i = 0; i < separated.size(); ++i ) {
         values.push_back(stoi_with_check(separated.at(i)));
     }
     return values;
+}
+
+
+// Checks if the path is clear.
+// Returns true if path is clear and false if not.
+// Will check in three parts:
+// - First if the path is clear to the second column.
+// - Second if the path is clear to the correct row.
+// - Third if the path is clear to the correct column.
+bool is_clear_path(vector<vector<Slot_type>>& board, const vector< unsigned int >& input)
+{
+    int distance_from_y_axis = 2 - input.at(0);
+    bool path_to_second;
+    // Checks path on the initial axis.
+    // path_to_second will be true if path is clear to the second column.
+    if ( distance_from_y_axis == 0 ) {
+        // you are already on the y-axis
+        path_to_second = true;
+    } else if ( ( distance_from_y_axis == 1 ) or
+                ( distance_from_y_axis == -1 ) ) {
+        // you are right next to the y-axis
+        if ( board.at(input.at(1) - 1).at(1) == EMPTY ) {
+            path_to_second = true;
+        } else {
+            path_to_second = false;
+        }
+    } else {
+        // you are on either places of the 4th column
+        if ( input.at(1) == input.at(3) and
+             input.at(2) == 3 ) {
+            // Special case when there is no need to go to the 2nd column at all.
+            if ( board.at(input.at(1) - 1).at(2) == EMPTY ) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if  (( board.at(input.at(1) - 1).at(1) == EMPTY ) and
+                 ( board.at(input.at(1) - 1).at(2) == EMPTY )) {
+                 path_to_second = true;
+            } else {
+                 path_to_second = false;
+            }
+        }
+    }
+
+    for ( unsigned int i : input ) {
+        cout << i << endl;
+    }
+
+    bool path_to_row;
+    if ( input.at(1) == input.at(3) ) {
+        // No need to move vertically
+        path_to_row = true;
+    } else if ( input.at(1) < input.at(3) ) {
+        // Go down. Iterates trough the spaces and returns false if at any point the path is blocked.
+        for ( unsigned int i = input.at(1); i <= input.at(3) - 1; ++i ) {
+            if ( board.at(i).at(1) != EMPTY ) {
+                return false;
+            } else {
+                path_to_row = true;
+            }
+        }
+    } else {
+        // Go up. Iterates trough the spaces and returns false if at any point the path is blocked.
+        if ( input.at(1) - input.at(3) == 1 ) {
+            path_to_row = true;
+        } else {
+            for ( unsigned int i = input.at(1) - 2; i >= input.at(3); --i ) {
+                if ( board.at(i).at(1) != EMPTY ) {
+                    return false;
+                } else {
+                    path_to_row = true;
+                }
+            }
+        }
+    }
+
+    bool path_to_column;
+    if ( input.at(2) == 2 ) {
+        // No need to move horizontally
+        path_to_column = true;
+    } else if ( input.at(2) < 2 ) {
+        // Move left
+        for ( int i = 0; i >= 0; --i ) {
+            if ( board.at(input.at(3) - 1).at(i) != EMPTY) {
+                return false;
+            } else {
+                path_to_column = true;
+            }
+        }
+    } else {
+        // Move right
+        for ( unsigned int i = 2; i <= input.at(2) - 1; ++i ) {
+            if ( board.at(input.at(3) - 1).at(i) != EMPTY) {
+                return false;
+            } else {
+                path_to_column = true;
+            }
+        }
+    }
+
+
+    if ( path_to_second and path_to_row and path_to_column ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+// After given valid input, moves the pieces to assigned location on board.
+// Checks the path before moving.
+// Will print out error and end function if the path is not clear.
+void move(vector<vector<Slot_type>>& board, const vector< unsigned int >& input)
+{
+    // Stub
 }
 
 
@@ -137,7 +254,7 @@ int main()
     };
     print(playground);
 
-    vector< int > values;
+    vector< unsigned int > values;
 
     while ( true ) {
         string line;
@@ -152,15 +269,17 @@ int main()
         }
 
         // Checks that the values are in range.
-        if ( values.at(0) <= 0 or values.at(0) > 4 or
-             values.at(1) <= 0 or values.at(1) > 5 or
-             values.at(2) <= 0 or values.at(2) > 4 or
-             values.at(3) <= 0 or values.at(3) > 5 ) {
+        if ( values.at(0) <= 0 or values.at(0) > COLUMS or
+             values.at(1) <= 0 or values.at(1) > ROWS or
+             values.at(2) <= 0 or values.at(2) > COLUMS or
+             values.at(3) <= 0 or values.at(3) > ROWS ) {
             cout << INVALID_POINT << endl;
             continue;
         }
 
         // Checks if the given points are valid.
+        // -1 in if statements because the values are the board values
+        // and must be turned to indexes.
         if ( playground.at(values.at(1) - 1).at(values.at(0) - 1) != GREEN and
              playground.at(values.at(1) - 1).at(values.at(0) - 1) != RED ) {
             cout << INVALID_POINT << endl;
@@ -168,6 +287,13 @@ int main()
         } else if ( playground.at(values.at(3) - 1).at(values.at(2) - 1) != EMPTY ) {
             cout << INVALID_POINT << endl;
             continue;
+        } else {
+            // Checks if there is a path from start to end.
+            if ( not is_clear_path(playground, values) ) {
+                continue;
+            } else {
+                // move
+            }
         }
     }
     return 0;
